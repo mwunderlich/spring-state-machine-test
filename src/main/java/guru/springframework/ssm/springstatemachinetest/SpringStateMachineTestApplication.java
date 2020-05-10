3387f9195ce63167b9277ab14be42b233807a8a0
mwunderlich <martin@wunderlich.com>
2020-05-10 20:11:13 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/ssm/springstatemachinetest/SpringStateMachineTestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.ssm.springstatemachinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStateMachineTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStateMachineTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/ssm/springstatemachinetest/SpringStateMachineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.ssm.springstatemachinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
